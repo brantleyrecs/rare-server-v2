377a02478c5af84654e7eff0891e4138a7363d30
Michael Brantley <brantleyrecs@gmail.com>
2023-12-13 20:45:10 -0600

    changed comment model

--- rareapi/models/comment.py
+++ rareapi/models/comment.py
@@ -3,7 +3,6 @@ from .user import User
 
 class Comment(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
   content = models.TextField()
   created_on = models.DateField()
-  time = models.TimeField()
-  post = models.ForeignKey(Post, on_delete=models.CASCADE)
